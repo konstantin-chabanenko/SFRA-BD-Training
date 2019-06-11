d82409568b34d37900445e690b21afce28f748ce
Konstantin Chabanenko <konstantin.chabanenko@osf-commerce.com>
2019-06-11 14:43:31 +0300

    [CEMT-639][CLONE - 5.4 Site Genesis - Cart - email message]: Cart.js cartridge was extended

--- cartridges/app_training/cartridge/controllers/Cart.js
+++ cartridges/app_training/cartridge/controllers/Cart.js
@@ -1,5 +1,38 @@
 "use strict";
 
+function sendProductEmail(email, productId) {
+    var ProductMgr = require("dw/catalog/ProductMgr");
+    var Resource = require('dw/web/Resource');
+    var Mail = require('dw/net/Mail');
+    var Template = require('dw/util/Template');
+    var Site = require('dw/system/Site');
+    var HashMap = require('dw/util/HashMap');
+
+    var product = ProductMgr.getProduct(productId);
+    var t = 0;
+    var objForProduct = {
+        productImage: product.getImage(),
+        productId: product.getId(),
+        productDescription: product.getLongDescription() 
+    };
+    var context = new HashMap();
+    // Object.keys(objForProduct).forEach(function(key) {
+    //     context.put(key, objForProduct[key])
+    // });
+    context.put("productImage", objForProduct.productImage);
+    context.put("productId", objForProduct.productId);
+    context.put("productDescription", objForProduct.productDescription);
+    var productEmail = new Mail();
+    productEmail.addTo(email);
+    productEmail.setFrom(Site.current.getCustomPreferenceValue('customerServiceEmail')
+    || 'no-reply@salesforce.com');
+    productEmail.setSubject(Resource.msg("email.subject", "cart", null));
+    var template = new Template('mail.isml');
+    var content = template.render(context).text;
+    productEmail.setContent(content, 'text/html', 'UTF-8');
+    productEmail.send();
+}
+
 var server = require("server");
 server.extend(require("../../../app_storefront_base/cartridge/controllers/Cart"));
 
@@ -14,6 +47,17 @@ server.getRoute("Show").append(function(req, res, next) {
         thresholdPrice: thresholdPrice,
         grandTotal: grandTotal
     });
+    next();
+});
+
+server.getRoute("AddProduct").append(function(req, res, next) {
+    var BasketMgr = require('dw/order/BasketMgr');
+    var customer = BasketMgr.currentBasket.customer;
+    var email = customer.profile.email;
+    var productId = req.form.pid;
+    if (email !== null) {
+        sendProductEmail(email, productId);
+    }
     next();
 });
 
